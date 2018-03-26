f3114e1997aaf6ed32ab1ab6c14a7fbc69ec9e2c
Eduardo Gonzalez <edugonch@gmail.com>
2018-03-25 22:06:39 -0600

    Replace autoload_paths with eager_load_paths

--- config/application.rb
+++ config/application.rb
@@ -31,7 +31,9 @@ module TwitterFetch
     # Skip views, helpers and assets when generating a new resource.
     config.api_only = true
 
-    config.autoload_paths << Rails.root.join('lib')
-    config.autoload_paths << Rails.root.join('app/services')
+    #config.autoload_paths << Rails.root.join('lib')
+    #config.autoload_paths << Rails.root.join('app/services')
+    config.eager_load_paths << Rails.root.join('lib')
+    config.eager_load_paths << Rails.root.join('app/services')
   end
 end
