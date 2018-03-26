398c0013a6f592cbc55d275abeb0b4aec116cc68
Eduardo Gonzalez <edugonch@gmail.com>
2018-03-25 21:46:45 -0600

    Add redis togo to app

--- config/initializers/sidekiq.rb
+++ config/initializers/sidekiq.rb
@@ -1,7 +1,17 @@
-Sidekiq.configure_server do |config|
-  config.redis = { url: 'redis://localhost:6379/12' }
-end
+if Rails.env.production?
+  Sidekiq.configure_server do |config|
+    config.redis = { url: ENV['REDISTOGO_URL'] }
+  end
 
-Sidekiq.configure_client do |config|
-  config.redis = { url: 'redis://localhost:6379/12' }
+  Sidekiq.configure_client do |config|
+    config.redis = { url: ENV['REDISTOGO_URL'] }
+  end
+else 
+  Sidekiq.configure_server do |config|
+    config.redis = { url: 'redis://localhost:6379/12' }
+  end
+
+  Sidekiq.configure_client do |config|
+    config.redis = { url: 'redis://localhost:6379/12' }
+  end
 end
